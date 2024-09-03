9df894275309a2719dfb783937b58aca2137974d
OwenFernandez9 <9357@holbertonstudents.com>
2024-09-03 10:26:34 -0300

    debugging factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,17 +8,5 @@ def factorial(n):
         n -= 1  # Decrementa n en cada iteración
     return result
 
-if len(sys.argv) != 2:
-    print("Uso: python script.py <número>")
-    sys.exit(1)
-
-try:
-    number = int(sys.argv[1])
-    if number < 0:
-        raise ValueError("El número debe ser no negativo")
-except ValueError as e:
-    print(f"Error: {e}")
-    sys.exit(1)
-
 f = factorial(number)
-print(f)
+print(f"{f}")
